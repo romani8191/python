7144e74ad8beb0bcbb49decfd0976598a0161e38
romani8191 <95995818+romani8191@users.noreply.github.com>
2023-05-28 13:24:46 +0530

    Update dice roll (2023_05_28 06_55_51 UTC).py

--- dice roll (2023_05_28 06_55_51 UTC).py
+++ dice roll (2023_05_28 06_55_51 UTC).py
@@ -34,3 +34,22 @@ ice_art = {
         "│  ●   ●  │",
         "└─────────┘")
 }
+
+
+dice=[]
+total=0
+num_of_dice=int(input("how many dice?"))
+
+for die in range(num_of_dice):
+    a=random.randint(1,6)
+    dice.append(a)
+
+print (dice)
+
+for die in range(num_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+for die in dice:
+    total=total+die
+print (f"total: {total}")
